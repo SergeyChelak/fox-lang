94e9118425c5b495bd3ad0135760268e6359e646
Sergey Chelak <sergey.chelak@gmail.com>
2025-07-24 10:00:14 +0300

    moved token to its module

--- src/fox/mod.rs
+++ src/fox/mod.rs
@@ -2,127 +2,12 @@ mod error;
 mod expression;
 mod parser;
 mod scanner;
-use std::fmt::Display;
+mod token;
 
 pub use error::*;
 pub use expression::*;
 pub use parser::*;
 pub use scanner::*;
+pub use token::*;
 
 pub type Source = [char];
-
-#[derive(Debug, Clone)]
-pub struct Token {
-    token_type: TokenType,
-    lexeme: String,
-    literal: Object,
-    code_location: CodeLocation,
-}
-
-impl Token {
-    pub fn new(
-        token_type: TokenType,
-        lexeme: &str,
-        literal: Object,
-        code_location: CodeLocation,
-    ) -> Self {
-        Self {
-            token_type,
-            lexeme: lexeme.to_string(),
-            literal,
-            code_location,
-        }
-    }
-
-    pub fn is_eof(&self) -> bool {
-        matches!(self.token_type, TokenType::Eof)
-    }
-}
-
-#[derive(Debug, Clone)]
-pub enum Object {
-    Nil,
-    Double(f32),
-    String(String),
-    Bool(bool),
-}
-
-impl Display for Object {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Nil => write!(f, "nil"),
-            Self::Double(value) => write!(f, "{}", value),
-            Self::String(value) => write!(f, "{}", value),
-            Self::Bool(value) => write!(f, "{}", value),
-        }
-    }
-}
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub enum TokenType {
-    // single-character tokens
-    LeftParenthesis,
-    RightParenthesis,
-    LeftBrace,
-    RightBrace,
-    Comma,
-    Dot,
-    Minus,
-    Plus,
-    Semicolon,
-    Slash,
-    Star,
-    // 1 or 2 chars tokens
-    Bang,
-    BangEqual,
-    Equal,
-    EqualEqual,
-    Greater,
-    GreaterEqual,
-    Less,
-    LessEqual,
-    // literals
-    Identifier,
-    String,
-    Number,
-    // Keywords
-    And,
-    Class,
-    Else,
-    False,
-    Fun,
-    For,
-    If,
-    Nil,
-    Or,
-    Print,
-    Return,
-    Super,
-    This,
-    True,
-    Var,
-    While,
-    //
-    Eof,
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct CodeLocation {
-    line: usize,
-    abs_position: usize,
-}
-
-impl CodeLocation {
-    pub fn new(line: usize, abs_position: usize) -> Self {
-        Self { line, abs_position }
-    }
-}
-
-impl Default for CodeLocation {
-    fn default() -> Self {
-        Self {
-            line: 1,
-            abs_position: 0,
-        }
-    }
-}
